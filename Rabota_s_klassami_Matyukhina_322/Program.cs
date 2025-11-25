9c1f733860c96396a0e070ac2a3a4547b407a981
Юлиана <mini-matilda49@bk.ru>
2025-11-25 05:06:05 -0100

    Добавьте файлы проекта.

---
+++ Rabota_s_klassami_Matyukhina_322/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabota_s_klassami_Matyukhina_322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
